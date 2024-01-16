513d1e951ce68b9fa013c06b8640b745eb350e4c
artprange <arthur.prange@gmail.com>
2024-01-16 20:16:46 -0300

    status bar styling

--- imHere/App.tsx
+++ imHere/App.tsx
@@ -1,10 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
 import Home from "./src/screens/Home";
+
 
 export default function App() {
   return(
 
+    /*statusbar não precisa no iphone, mas no android sim*/
+
+    <>
+      <StatusBar
+      style="light"
+      backgroundColor='transparent'
+      translucent
+    
+       /> 
+
     <Home />
-   
+    </>
 
   )
  
